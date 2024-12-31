6af569d6c850c499eeae8bf1ebb7b77a6d56e2c6
Peter Chege <peterkagure@gmail.com>
2025-01-01 01:04:18 +0300

    Update dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,21 +1,22 @@
-import com.android.build.gradle.internal.cxx.configure.gradleLocalProperties
 plugins {
-    id ("com.android.application")
-    id ("org.jetbrains.kotlin.android")
-    id ("org.jetbrains.kotlin.plugin.serialization")
+    id("com.android.application")
+    kotlin("android")
+    kotlin("plugin.serialization")
+    kotlin("plugin.compose")
+    id("com.google.devtools.ksp")
+    id ("com.google.dagger.hilt.android")
+    id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin")
 }
 
-val apiKey: String = gradleLocalProperties(rootDir).getProperty("API_KEY")
-
 android {
-    namespace =  "com.peterchege.composenewsapp"
-    compileSdk = 34
+    namespace = "com.peterchege.composenewsapp"
+    compileSdk = 35
 
     defaultConfig {
-        applicationId =  "com.peterchege.composenewsapp"
+        applicationId = "com.peterchege.composenewsapp"
         minSdk = 21
-        targetSdk =  34
-        versionCode =  1
+        targetSdk = 35
+        versionCode = 1
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
@@ -28,98 +29,102 @@ android {
         arg("room.schemaLocation", "$projectDir/schemas")
     }
     buildTypes {
-        getByName("debug"){
-            buildConfigField("String", "API_KEY", apiKey)
+        getByName("debug") {
+
         }
         getByName("release") {
-            buildConfigField("String", "API_KEY", apiKey)
-            isMinifyEnabled =  false
-            proguardFiles (getDefaultProguardFile("proguard-android-optimize.txt"), "proguard-rules.pro")
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
     }
     compileOptions {
-        sourceCompatibility =  JavaVersion.VERSION_17
-        targetCompatibility =  JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = JavaVersion.VERSION_17.toString()
     }
     buildFeatures {
-        compose =  true
+        compose = true
+        buildConfig = true
     }
-    composeOptions {
-        kotlinCompilerExtensionVersion =  "1.5.3"
-    }
-    packagingOptions {
+
+    packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
         }
     }
 }
 
+composeCompiler {
+    reportsDestination = layout.buildDirectory.dir("compose_compiler")
+}
+
 dependencies {
 
-    implementation ("androidx.core:core-ktx:1.10.1")
-    implementation ("androidx.activity:activity-compose:1.7.2")
-    implementation ("androidx.compose.ui:ui:1.6.0-alpha04")
-    implementation ("androidx.compose.ui:ui-tooling-preview:1.6.0-alpha04")
-    implementation ("androidx.compose.material:material:1.6.0-alpha04")
-    testImplementation ("junit:junit:4.13.2")
-    androidTestImplementation ("androidx.test.ext:junit:1.1.5")
-    androidTestImplementation ("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation ("androidx.compose.ui:ui-test-junit4:1.6.0-alpha04")
-    debugImplementation ("androidx.compose.ui:ui-tooling:1.6.0-alpha04")
-    debugImplementation ("androidx.compose.ui:ui-test-manifest:1.6.0-alpha04")
+    implementation("androidx.core:core-ktx:1.15.0")
+    implementation("androidx.activity:activity-compose:1.9.3")
+    implementation("androidx.compose.ui:ui:1.7.6")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.7.6")
+    implementation("androidx.compose.material:material:1.7.6")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+    androidTestImplementation("androidx.compose.ui:ui-test-junit4:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-tooling:1.7.6")
+    debugImplementation("androidx.compose.ui:ui-test-manifest:1.7.6")
 
-    implementation ("androidx.constraintlayout:constraintlayout-compose:1.0.1")
-    implementation ("androidx.compose.material:material-icons-extended:1.6.0-alpha04")
-    implementation ("androidx.navigation:navigation-compose:2.7.1")
+    implementation("androidx.constraintlayout:constraintlayout-compose:1.1.0")
+    implementation("androidx.compose.material:material-icons-extended:1.7.6")
+    implementation("androidx.navigation:navigation-compose:2.8.5")
 
     // coil
-    implementation ("io.coil-kt:coil-compose:2.4.0")
+    implementation("io.coil-kt:coil-compose:2.7.0")
 
     // retrofit
-    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:retrofit:2.11.0")
     implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation ("com.squareup.okhttp3:okhttp:5.0.0-alpha.2")
-    implementation ("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.2")
-    implementation ("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
+    implementation("com.squareup.okhttp3:okhttp:5.0.0-alpha.14")
+    implementation("com.squareup.okhttp3:logging-interceptor:5.0.0-alpha.14")
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.6.0")
 
     // view model
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation ("androidx.lifecycle:lifecycle-runtime-compose:2.6.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
+    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.8.7")
 
     //coroutines
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
-    implementation ("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.9.0")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.9.0")
 
-    implementation("androidx.paging:paging-compose:3.2.0")
-    implementation("androidx.paging:paging-runtime-ktx:3.2.0")
-
+    implementation("androidx.paging:paging-compose:3.3.5")
+    implementation("androidx.paging:paging-runtime-ktx:3.3.5")
 
     // datastore (core and preferences)
-    implementation ("androidx.datastore:datastore:1.0.0")
-    implementation("androidx.datastore:datastore-preferences:1.0.0")
+    implementation("androidx.datastore:datastore:1.1.1")
+    implementation("androidx.datastore:datastore-preferences:1.1.1")
 
     // dagger hilt
-    implementation ("com.google.dagger:hilt-android:2.48")
-    ksp("com.google.dagger:dagger-compiler:2.48") // Dagger compiler
-    ksp("com.google.dagger:hilt-compiler:2.48")   // Hilt compiler
-    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation("com.google.dagger:hilt-android:2.53.1")
+    ksp("com.google.dagger:dagger-compiler:2.53.1") // Dagger compiler
+    ksp("com.google.dagger:hilt-compiler:2.53.1")   // Hilt compiler
+    implementation("androidx.hilt:hilt-navigation-compose:1.2.0")
 
     // room
-    implementation( "androidx.room:room-runtime:2.5.2")
-    ksp ("androidx.room:room-compiler:2.5.2")
-    implementation ("androidx.room:room-ktx:2.5.2")
-    implementation ("androidx.room:room-paging:2.5.2")
+    implementation("androidx.room:room-runtime:2.6.1")
+    ksp("androidx.room:room-compiler:2.6.1")
+    implementation("androidx.room:room-ktx:2.6.1")
+    implementation("androidx.room:room-paging:2.6.1")
 
     implementation("com.jakewharton.timber:timber:5.0.1")
-    debugImplementation ("com.squareup.leakcanary:leakcanary-android:2.12")
+    debugImplementation("com.squareup.leakcanary:leakcanary-android:2.14")
 
     // chucker
-    debugImplementation ("com.github.chuckerteam.chucker:library:4.0.0")
-    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.0.0")
+    debugImplementation("com.github.chuckerteam.chucker:library:4.1.0")
+    releaseImplementation("com.github.chuckerteam.chucker:library-no-op:4.1.0")
 
 
 }
--- build.gradle.kts
+++ build.gradle.kts
@@ -1,23 +1,15 @@
 plugins {
+    id("com.android.application") version "8.7.3" apply false
+    kotlin("android") version "2.1.0" apply false
+    kotlin("jvm") version "2.1.0" apply false
+    kotlin("plugin.compose") version "2.1.0" apply false
+    kotlin("plugin.serialization") version "2.1.0" apply false
+    id ("com.google.dagger.hilt.android") version "2.53.1" apply false
     id("com.diffplug.spotless") version "5.3.0"
-    id ("org.jetbrains.kotlin.jvm") version "1.9.0"
-    id("com.google.devtools.ksp") version "1.9.10-1.0.13"
-}
-subprojects{
-    apply(plugin = "com.google.devtools.ksp")
-    apply(plugin = "dagger.hilt.android.plugin")
+    id("com.google.devtools.ksp") version "2.1.0-1.0.29" apply false
+    id("com.google.gms.google-services") version "4.4.2" apply false
 }
 
-buildscript {
-
-    dependencies {
-        classpath ("com.android.tools.build:gradle:7.4.2")
-        classpath ("com.google.dagger:hilt-android-gradle-plugin:2.48")
-        classpath ("com.google.gms:google-services:4.3.14")
-        classpath ("org.jetbrains.kotlin:kotlin-gradle-plugin:1.9.10")
-        classpath ("org.jetbrains.kotlin:kotlin-serialization:1.9.10")
-    }
-}
 
 apply(plugin = "com.diffplug.spotless")
 spotless {
@@ -27,6 +19,12 @@ spotless {
             rootProject.file("${project.rootDir}/spotless/LICENSE.txt"),
             "^(package|object|import|interface)"
         )
+    }
+}
+
+buildscript {
+    dependencies {
+        classpath("com.google.android.libraries.mapsplatform.secrets-gradle-plugin:secrets-gradle-plugin:2.0.1")
     }
 }
 
